1a475dae0ae195bcd32a617665886a9cbd0b826e
DESKTOP-DGJS3IN\navee <naveendrarashmika@gmail.com>
2022-10-18 21:58:33 +0530

    Add sounds to the webpage

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var noOfElements = document.querySelectorAll(".drum").length;
+var audio = new Audio('sounds/tom-1.mp3');
 
 for (var i = 0; i<noOfElements; i++) {
 	document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-		window.alert("I got clicked");
+		audio.play();
 });
 }
 
